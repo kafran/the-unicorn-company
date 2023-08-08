46c30e2d395e91cbc659f6163102298887bcf9a2
Kolmar Kafran <kolmar@kafran.net>
2023-08-08 20:13:48 -0300

    Release website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-pink": "#d1367e",
+      }
+    },
+  },
+  plugins: [],
+}
+
